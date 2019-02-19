b18eebaa9c48ca375c2b22f9d00fe5647e1aa663
Paweł Skórzewski <pawel.skorzewski@amu.edu.pl>
2019-02-19 09:58:54 +0100

    Tools for annotating

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ AMUSED – Adam Mickiewicz University's tools for Sentiment analysis and Emotion
 
 ## License
 MIT
+
+## Gold standard
+
+You can help creating gold standard corpus by annotating batches of selected sentences with provied script `annotate.py`:
+```
+./annotate.py corpora/wl-test.100b<BATCH_NO>.txt corpora/wl-test.100b<BATCH_NO>a<ANNOTATION_NO>.tsv
+```
+e.g.:
+```
+./annotate.py corpora/wl-test.100b1.txt corpora/wl-test.100b1a1.tsv
+```
--- bnd2text.py
+++ bnd2text.py
@@ -4,6 +4,7 @@
 
 import argparse
 import math
+import random
 import re
 import sys
 
@@ -36,19 +37,30 @@ def parse_arguments():
         default=math.inf,
         help='Maximum number of utterances converted',
     )
+    parser.add_argument(
+        '-s', '--sample',
+        type=float,
+        default=1.0,
+        help='Sample randomly a given fraction of utterances',
+    )
     return parser.parse_args()
 
 
-def main(bnd, txt, max_utt=math.inf):
+def main(bnd, txt, max_utt=math.inf, sample=1.0):
     with BNDReader(bnd) as reader:
-        for i, par in enumerate(reader):
-            if i > max_utt:
+        utterances_collected = 0
+        for par in reader:
+            if utterances_collected > max_utt:
                 break
+            if random.random() > sample:
+                continue
             utterance = RE_PUNCT.sub(
                 r'\1', ' '.join(row['word'] for row in par))
             print(utterance, file=txt)
+            utterances_collected += 1
 
 
 if __name__ == '__main__':
     args = parse_arguments()
-    main(args.infile, args.outfile, max_utt=args.max)
+    main(args.infile, args.outfile,
+         max_utt=args.max, sample=args.sample)
