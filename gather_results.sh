e0bc2752e5389973cec461f747fd2c4da9449ebf
Paweł Skórzewski <pawel.skorzewski@amu.edu.pl>
2019-04-06 16:52:55 +0200

    Experiment with zero function

--- gather_results.sh
+++ gather_results.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-grep RMSE experiment_results/*reporting_clauses* | awk 'BEGIN {FS=OFS=":"} {print $3 "\t" $1}' | sort -n
+grep RMSE experiment_results/* | awk 'BEGIN {FS=OFS=":"} {print $3 "\t" $1}' | sort -n
